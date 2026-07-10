c869dc78c0b94411caba3af4ddbf9cdd6dfd8b6e
temirovv <temirovv21@gmail.com>
2026-07-10 10:36:49 +0500

    brand and category viewsets added to router

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -1,8 +1,12 @@
 from rest_framework.routers import DefaultRouter
-from .views import BannerModelViewSet
+from .views import (
+    BannerModelViewSet, CategoryReadOnlyModelViewSet,
+    BrandReadOnlyModelViewSet)
 
 
 router = DefaultRouter()
-router.register(r'banners', BannerModelViewSet)
+router.register(r'banners', BannerModelViewSet, basename='banner')
+router.register(r'categories', CategoryReadOnlyModelViewSet, basename='category')
+router.register(r'brands', BrandReadOnlyModelViewSet, basename='brand')
 
 urlpatterns = router.urls
