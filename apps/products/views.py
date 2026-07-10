f93fd6f53833e7e57a39bc4e7dd22b4508cdb703
temirovv <temirovv21@gmail.com>
2026-07-10 10:36:22 +0500

    Category and Brand views implemented

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,11 +1,30 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 from rest_framework.permissions import AllowAny
+from django.db.models import Count
 
-from .models import Banner
-from .serializers import BannerSerializer
+from .models import Banner, Category, Brand
+from .serializers import BannerSerializer, CategorySerializer, BrandSerializer
 
 
-class BannerModelViewSet(ModelViewSet):
+class BannerModelViewSet(ReadOnlyModelViewSet):
     serializer_class = BannerSerializer
-    queryset = Banner.objects.all()
+    queryset = Banner.objects.filter(is_active=True)
     permission_classes = [AllowAny]
+
+
+class CategoryReadOnlyModelViewSet(ReadOnlyModelViewSet):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.filter(is_active=True)
+    permission_classes = [AllowAny]
+
+
+class BrandReadOnlyModelViewSet(ReadOnlyModelViewSet):
+    serializer_class = BrandSerializer
+    permission_classes = [AllowAny]
+
+    def get_queryset(self):
+        return (
+            Brand.objects.filter(is_active=True).annotate(
+                products_count=Count('products')
+            )        
+        )
